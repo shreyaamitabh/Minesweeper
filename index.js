16cbe6e1c5903a3eb668375b48547ee85baa2cda
shreyaamitabh <52948891+shreyaamitabh@users.noreply.github.com>
2020-06-05 15:23:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -76,7 +76,7 @@ for(let i=0; i<9; i++)
                     
                     document.getElementById(arr[y]).style.backgroundColor="red";
                     document.getElementById(arr[y]).style.verticalAlign="20px";
-                    document.getElementById(arr[y]).innerHTML="💣f";
+                    document.getElementById(arr[y]).innerHTML="💣";
                     over=true;
                 }
                 button.style.display="block";
